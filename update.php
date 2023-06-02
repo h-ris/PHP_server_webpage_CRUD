c2cbfaa9421a7f809e7ce130d3314c7dce83aa43
h-ris <95009925+h-ris@users.noreply.github.com>
2023-06-02 12:35:22 -0400

    Update update.php

--- update.php
+++ update.php
@@ -1,13 +1,5 @@
 <?php
 
-/* Name: Huixin Xu
-Due Date: Apr 10, 2023
-Section: CST8285 313
-Lab: 23W Assignment2
-File: update.php
-Lab objective: Create PHP server web pages corresponding to CRUD operations for one entity.
-*/
-
 // Include movieorderDAO file
 require_once('./movieDAO.php');
 
