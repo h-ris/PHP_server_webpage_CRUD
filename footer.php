8da0da96e103550be3fadac288a44e54e41b47e6
h-ris <95009925+h-ris@users.noreply.github.com>
2023-06-02 12:34:00 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,3 @@
-<!-- Name: Huixin Xu
-Due Date: Apr 10, 2023
-Section: CST8285 313
-Lab: 23W Assignment2
-File: footer.php
-Lab objective: Create PHP server web pages corresponding to CRUD operations for one entity.
--->
 
 <footer>
         <p>&copy; <?php echo date("Y");?>  CST8285.
