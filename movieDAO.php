998896409a75ee30a086456c315e283372ec08bd
h-ris <95009925+h-ris@users.noreply.github.com>
2023-06-02 12:34:44 -0400

    Update movieDAO.php

--- movieDAO.php
+++ movieDAO.php
@@ -1,13 +1,5 @@
 <?php
 
-/* Name: Huixin Xu
-Due Date: Apr 10, 2023
-Section: CST8285 313
-Lab: 23W Assignment2
-File: movieDAO.php
-Lab objective: Create PHP server web pages corresponding to CRUD operations for one entity.
-*/
-
 require_once('abstractDAO.php');
 require_once('./movie.php');
 
